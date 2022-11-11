604b476259f1fabb485c1f34937ccd057f6d9737
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-11 21:00:44 +0900

    done

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -6,10 +6,10 @@ def find_max_odd(data):
     returns: maximum odd number in the list
     """
     max = data[0]
+    max = 0
     for i in data:
         if i > max:
             max = i
-        odd = 0
         if max % 2 != 0:
             odd = max
     return odd
