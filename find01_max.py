9c99846dfaf0db958049025c2dd7674bbeada972
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-11 20:33:15 +0900

    done

--- find01_max.py
+++ find01_max.py
@@ -5,5 +5,8 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
-    
+    max = data[0]
+    for i in data:
+        if i > max:
+            max = i
+    return max    
